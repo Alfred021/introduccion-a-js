6070b3e247d5e3d9a096e6051766fe3455dcc73b
Alfred021 <alfredomedina10@hotmail.com>
2019-12-08 21:45:11 -0300

    Tarea 2, Clase 6
    Tarea 2, Clase 6

---
+++ tareas/clase-6/tarea 2, clase 6.js
@@ -0,0 +1,93 @@
+document.querySelector('#agregar').onclick = function () {
+    agregarMiembro();
+    return false;
+}
+
+document.querySelector('#quitar').onclick = function () {
+    quitarMiembro ();  
+    return false;
+}
+
+document.querySelector('#calcular').onclick = function () {
+    document.querySelector('#mayor-salario-anual').textContent = mayorSalarioAnual ();
+    document.querySelector('#menor-salario-anual').textContent =   menorSalarioAnual ();
+    document.querySelector('#salario-anual-promedio').textContent = salarioAnualPromedio ();
+    document.querySelector('#salario-mensual-promedio').textContent = salarioMensualPromedio ();
+}
+
+
+function agregarMiembro() {
+
+    let contador = 1;
+    for (i = 0; i < document.querySelectorAll('.integrante').length; i++) {
+        contador += 1;
+    }
+
+    const div = document.createElement('div');
+    div.className = 'integrante';
+
+    const label = document.createElement('label');
+    label.textContent = "Salario Anual del integrante Nro: " + contador; 
+
+    const input = document.createElement('input');
+    input.type = 'number';
+
+    div.appendChild(label);
+    div.appendChild(input);
+
+    const miembrosFamilia = document.querySelector('#miembros-familia');
+    miembrosFamilia.appendChild(div);
+
+}
+
+function quitarMiembro () {
+    let miembrosaEliminar = document.querySelector('#miembros-familia');
+    let elementoEliminar = document.querySelectorAll('.integrante');
+    miembrosaEliminar.removeChild(elementoEliminar[elementoEliminar.length-1]);
+
+}
+
+function sacarSalarios () {
+    const salariosInputs = document.querySelectorAll('.integrante input');
+    const salariosMiembros = [];
+    for (i = 0; i < salariosInputs.length; i++) {
+        if (salariosInputs[i].value === "") {
+        continue;
+        }
+        salariosMiembros.push(Number(salariosInputs[i].value));    
+    }
+
+    return salariosMiembros;
+}
+
+function mayorSalarioAnual () {
+    let salarios = sacarSalarios();
+    let resultadoMayorSalarioAnual = Math.max(...salarios);
+    return resultadoMayorSalarioAnual;
+}
+
+function menorSalarioAnual () {
+    let salarios = sacarSalarios ();
+    let resultadoMenorSalarioAnual = Math.min(...salarios);
+    return resultadoMenorSalarioAnual;
+}
+
+function salarioAnualPromedio () {
+    let salarios = sacarSalarios ();
+    let sumaSalarios = 0;
+    for (i = 0; i < salarios.length; i++) {
+        sumaSalarios += salarios[i];
+    }
+    return sumaSalarios/ salarios.length;
+}
+
+function salarioMensualPromedio () {
+    let salarios = sacarSalarios ();
+    let sumaSalarios = 0;
+    let salarioMensualPromedio = 0;
+    for (i = 0; i < salarios.length; i++) {
+        sumaSalarios += salarios[i]/12
+    }
+    salarioMensualPromedio = sumaSalarios / salarios.length;
+    return salarioMensualPromedio;
+}
