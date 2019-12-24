bfa8c536ae6e73e5fbc4c6b4fd20ad0d3c9ba6ec
Alfred021 <alfredomedina10@hotmail.com>
2019-12-24 20:31:35 -0300

    Cambios sugeridos por fabricio
    Se corrigieron los nombres, se reescribieron algunas funciones y otros consejos en general de la revision de fabricio.

--- tareas/clase-6/tarea 1, clase 6.js
+++ tareas/clase-6/tarea 1, clase 6.js
@@ -1,38 +1,36 @@
 let miembrosGrupoFamiliar = Number(prompt('Cuantos miembros hay en el grupo familiar'));
-
-    for (i = 0; i< miembrosGrupoFamiliar; i++) {
-        CrearMiembros(i);
+    for (i = 0; i < miembrosGrupoFamiliar; i++) {
+        crearMiembros(i);
     }
 
 document.querySelector('#calcular').onclick = function() {
-    let edades = SacarEdades ();
-    document.querySelector('#mayor-edad').value = sacarmayorEdad ();
-    document.querySelector('#menor-edad').value = sacarmenorEdad ();
-    document.querySelector('#promedio-edad').value = sacarpromedioEdad(edades);
+    let edades = obtenerEdadesMiembros ();
+    document.querySelector('#mayor-edad').value = calcularMayorEdad(edades);
+    document.querySelector('#menor-edad').value = calcularMenorEdad(edades);
+    document.querySelector('#promedio-edad').value = calcularPromedioEdad(edades);
 
     return false;
 };
 
 document.querySelector('#reset').onclick = function () {
-    borrarIntegrantes ();
-    borrarnumMayor ();
-    borrarnumMenor ();
-    borrarPromedio ();
+    borrarIntegrantes();
+    borrarNumeroMayor();
+    borrarNumeroMenor();
+    borrarPromedio();
 
     return false
 };
  
-function CrearMiembros(numero = 1) {
+function crearMiembros(indice = 0) {
 
-    if (numero >= 1) {
-        acumulador = 0;
-        acumulador + 1;
-    }
+    if (indice >= 0) {
+        indice = indice + 1;
+    
     const div = document.createElement('div');
     div.className = 'integrante';
 
     const label = document.createElement('label');
-    label.textContent = 'Edad del integrante Nro: ' + (acumulador);
+    label.textContent = 'Edad del integrante Nro: ' + (indice);
 
     const input = document.createElement('input');
     input.type = 'number';
@@ -42,9 +40,10 @@ function CrearMiembros(numero = 1) {
 
     const miembros = document.querySelector('#miembros');
     miembros.appendChild(div);
+    }
 }
 
-function SacarEdades () {
+function obtenerEdadesMiembros() {
     const edadesinputs = document.querySelectorAll('.integrante input');
     const edadesMiembros = [];
     for (let j = 0; j < edadesinputs.length; j++) {
@@ -53,41 +52,48 @@ function SacarEdades () {
     return edadesMiembros;
 }
 
-function sacarmayorEdad () {
-    let mayorEdad = SacarEdades ();
-    let resultadomayorEdad = Math.max(...mayorEdad);
-    return resultadomayorEdad;
+function calcularMayorEdad(edades) {
+    let mayorNumero = edades[0];
+    for (i = 1; i > edades.length; i++) {
+        if (edades[i] > mayorNumero ) {
+            mayorNumero = edades[i];
+        }
+    }    
+    return mayorNumero;
 }
 
-function sacarmenorEdad () {
-    let menorEdad = SacarEdades ();
-    let resultadomenorEdad = Math.min(...menorEdad);
-    return resultadomenorEdad;
-}
-
-function sacarpromedioEdad(edades) {
-    let contador = 0;
-    for (i = 0; i < edades.length; i++){
-        contador += edades[i];
+function calcularMenorEdad(edades) {
+    let menorNumero = edades[0];
+    for (i = 1; i < edades.length; i++) {
+        if (edades[i] < menorNumero) {
+            menorNumero = edades[i];
+        }
     }
-    return contador / edades.length;
+    return menorNumero;
 }
 
-function borrarnumMayor () {
+function calcularPromedioEdad(edades) {
+    let acumulador = 0;
+    for (i = 0; i < edades.length; i++){
+        acumulador += edades[i];
+    }
+    return acumulador / edades.length;
+}
+
+function borrarNumeroMayor() {
     document.getElementById('mayor-edad').value = "";
 }
 
-function borrarnumMenor () {
+function borrarNumeroMenor() {
     document.getElementById('menor-edad').value = "";
 }
 
-function borrarPromedio () {
+function borrarPromedio() {
     document.getElementById('promedio-edad').value = "";
 }
 
-function borrarIntegrantes () {
+function borrarIntegrantes() {
     let resetIntegrantes = document.querySelectorAll('.integrante input');
     for (i = 0; i < resetIntegrantes.length; i++) {
         resetIntegrantes[i].value = "";
-    }
-}
+}}
